023566d2d34433f32aba01ff1230026660a56a62
ThibaultRoger <thibaultroger1@gmail.com>
2019-03-22 15:01:51 +0100

    Update Email.php

--- Lib/Helpers/Email.php
+++ Lib/Helpers/Email.php
@@ -4,6 +4,47 @@ namespace Lib\Helpers;
 
 class Email {
   
+  public function email_validation ($email) {
+    
+    
+    
+  }
+  
+ public function subject () {
+   
+   
+ }
+  
+  
+ public function boundary() {
+   
+   
+   
+ }
+  
+  
+  public function header() {
+    
+    
+    
+  }
+  
+  
+  
+  
+  public function message($message) {
+    
+    
+    
+  }
+  
+  
+  public function send () {
+    
+    
+  }
+  
+  
   
 }
   
